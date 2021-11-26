871835efd7d376ef8d13c87a23607da624c88452
fangjiaxin <fangjiaxin@meituan.com>
2021-11-26 16:28:15 +0800

    week13

---
+++ src/main/java/leetcode/editor/en/NonOverlappingIntervals.java
@@ -0,0 +1,92 @@
+//Given an array of intervals intervals where intervals[i] = [starti, endi], ret
+//urn the minimum number of intervals you need to remove to make the rest of the i
+//ntervals non-overlapping. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+//Output: 1
+//Explanation: [1,3] can be removed and the rest of the intervals are non-overla
+//pping.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: intervals = [[1,2],[1,2],[1,2]]
+//Output: 2
+//Explanation: You need to remove two [1,2] to make the rest of the intervals no
+//n-overlapping.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: intervals = [[1,2],[2,3]]
+//Output: 0
+//Explanation: You don't need to remove any of the intervals since they're alrea
+//dy non-overlapping.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= intervals.length <= 105 
+// intervals[i].length == 2 
+// -5 * 104 <= starti < endi <= 5 * 104 
+// 
+// Related Topics Array Dynamic Programming Greedy Sorting 
+// 👍 3011 👎 84
+
+
+package leetcode.editor.en;
+
+import java.util.Arrays;
+
+public class NonOverlappingIntervals {
+    public static void main(String[] args) {
+        Solution solution = new NonOverlappingIntervals().new Solution();
+        System.out.println(solution.eraseOverlapIntervals(new int[][]{
+                {1, 2}, {2, 3}, {3, 4}, {1, 3}
+        }));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 边界条件比较敏感！！
+        public int eraseOverlapIntervals(int[][] intervals) {
+            // 先按照start升序，再按照end升序
+            Arrays.sort(intervals, (a, b) -> a[0] - b[0] == 0 ? a[1] - b[1] : a[0] - b[0]);
+            // 统计可以加进去的，初始值为1
+            int cnt = 1;
+            // 保留上一个被加进去的段，再和后面的做比较
+            int preStart = intervals[0][0];
+            int preEnd = intervals[0][1];
+
+            for (int i = 1; i < intervals.length; i++) {
+                int[] e = intervals[i];
+                // end升序，所以略过
+                if (preStart == e[0]) {
+                    continue;
+//                    大于start，但比end要小
+                } else if (preEnd > e[0] && preEnd <= e[1]) {
+                    continue;
+//                    更新到下一个分组，注意end即使一样也要更新start，方便下一个分组遍历
+                } else if (preEnd >= e[1]) {
+                    preStart = e[0];
+                    preEnd = e[1];
+                } else {
+                    cnt++;
+                    preStart = e[0];
+                    preEnd = e[1];
+                }
+            }
+            return intervals.length - cnt;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/editor/en/TwoBestNonOverlappingEvents.java
@@ -0,0 +1,95 @@
+//You are given a 0-indexed 2D integer array of events where events[i] = [startT
+//imei, endTimei, valuei]. The ith event starts at startTimei and ends at endTimei
+//, and if you attend this event, you will receive a value of valuei. You can choo
+//se at most two non-overlapping events to attend such that the sum of their value
+//s is maximized. 
+//
+// Return this maximum sum. 
+//
+// Note that the start time and end time is inclusive: that is, you cannot atten
+//d two events where one of them starts and the other ends at the same time. More 
+//specifically, if you attend an event with end time t, the next event must start 
+//at or after t + 1. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: events = [[1,3,2],[4,5,2],[2,4,3]]
+//Output: 4
+//Explanation: Choose the green events, 0 and 1 for a sum of 2 + 2 = 4.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: events = [[1,3,2],[4,5,2],[1,5,5]]
+//Output: 5
+//Explanation: Choose event 2 for a sum of 5.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: events = [[1,5,3],[1,5,1],[6,6,5]]
+//Output: 8
+//Explanation: Choose events 0 and 2 for a sum of 3 + 5 = 8. 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= events.length <= 105 
+// events[i].length == 3 
+// 1 <= startTimei <= endTimei <= 109 
+// 1 <= valuei <= 106 
+// 
+// Related Topics Array Binary Search Dynamic Programming Sorting Heap (Priority
+// Queue) 
+// 👍 229 👎 6
+
+
+package leetcode.editor.en;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeMap;
+
+public class TwoBestNonOverlappingEvents {
+    public static void main(String[] args) {
+
+        Solution solution = new TwoBestNonOverlappingEvents().new Solution();
+        System.out.println(solution.maxTwoEvents(new int[][]{
+                {66, 97, 90}, {98, 98, 68}, {38, 49, 63}, {91, 100, 42}, {92, 100, 22}, {1, 77, 50}, {64, 72, 97}
+        }));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxTwoEvents(int[][] events) {
+            // Arrays.sort是原地修改，Arrays.stream(events).sorted()返回新的列表
+            Arrays.sort(events, Comparator.comparingInt(value -> value[0]));
+            int res = 0;
+            // map记录key>start的最大weight
+            int max = 0;
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for (int i = events.length - 1; i >= 0; i--) {
+                int[] event = events[i];
+                int start = event[0], end = event[1], weight = event[2];
+                // ceilingKey找到大于等于value的值，所以要+1
+                res = Math.max(res, map.ceilingKey(end + 1) == null ? weight :
+                        map.get(map.ceilingKey(end + 1)) + weight);
+                max = Math.max(weight, max);
+                if (map.containsKey(start) && weight > map.get(start)) {
+                    map.put(start, max);
+                } else if (!map.containsKey(start)) {
+                    map.put(start, max);
+                }
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
